6e5bfe2f55128cdfb438bbc6057ebb7e8768dd52
Eduarda Albuquerque <albuquerque.zxl@gmail.com>
2024-08-25 22:45:32 -0300

    .

--- programasC/programa1.c
+++ programasC/programa1.c
@@ -4,6 +4,11 @@
 //biblioteca c para trabalhar com entrada e saida
 
 int main() {
-    
+    //declarando variaveis
+    int idade;
+
+    //função para escrever algo na saida(console)
+    printf("Qual é a sua idade? ");
+
     return 0;
 }
