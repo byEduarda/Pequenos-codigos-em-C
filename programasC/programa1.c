9db17ce6b3ebe46e4da076ceb7d353c3064b44d8
Eduarda Albuquerque <albuquerque.zxl@gmail.com>
2024-08-25 22:48:02 -0300

    oi

--- programasC/programa1.c
+++ programasC/programa1.c
@@ -3,9 +3,12 @@
 #include <stdio.h>
 //biblioteca c para trabalhar com entrada e saida
 
-int main() {
+int main() { // a chave delimita um bloco de codigo
     //declarando variaveis
-    int idade;
+    int idade;  //inteiro
+
+    //declaranndo e inicializando variaveis
+    //int idade = 0;
 
     //função para escrever algo na saida(console)
     printf("Qual é a sua idade? ");
